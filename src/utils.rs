643df25e7aa07b8e0371d7b13634fdb40feb10af
Nils Martel <nilsmartel@yahoo.de>
2026-07-06 11:32:21 +0200

    Document helper types

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,29 @@
 /// Trait for types that can be encoded as a sequence of `u64` values and decoded back.
+///
+/// Implement `encode` and `decode`; `compress` is provided for free.
+///
+/// # Example
+///
+/// ```rust
+/// # use vbyte::utils::VbyteEncode;
+/// struct Point { x: u32, y: u32 }
+///
+/// impl VbyteEncode for Point {
+///     fn encode(&self, out: &mut Vec<u64>) {
+///         self.x.encode(out);
+///         self.y.encode(out);
+///     }
+///     fn decode(fields: &[u64]) -> Result<(Self, &[u64]), &'static str> {
+///         let (x, fields) = u32::decode(fields)?;
+///         let (y, fields) = u32::decode(fields)?;
+///         Ok((Point { x, y }, fields))
+///     }
+/// }
+///
+/// let points = vec![Point { x: 1, y: 2 }, Point { x: 314, y: 159 }];
+/// let compressed = Point::compress(&points);
+/// let restored = compressed.decompress().unwrap();
+/// ```
 pub trait VbyteEncode: Sized {
     /// Append the u64 representation of `self` to `out`.
     fn encode(&self, out: &mut Vec<u64>);
@@ -21,6 +46,9 @@ pub trait VbyteEncode: Sized {
     }
 }
 
+/// A compressed sequence of values of type `T`.
+///
+/// Obtained via [`VbyteEncode::compress`] or the free [`compress`] function.
 pub struct CompressedList<T: VbyteEncode> {
     pub compressed_data: Vec<u8>,
     _ty: PhantomData<T>,
